d68e78c7467bf6425ee6d47b7ba09f4be64fc7ab
JochyDev <joseluisjimenez1998@gmail.com>
2022-10-26 15:07:51 -0400

    ADD: services added

---
+++ src/services/comment.service.js
@@ -0,0 +1,60 @@
+const BaseRepository = require("../repositories/base.repository");
+
+let _commetRepository = null;
+let _ideaRepository = null;
+
+
+class CommnetService extends BaseRepository{
+    constructor({ CommetRepository, IdeaRepository }){
+        super(CommetRepository);
+        _commetRepository = CommetRepository;
+        _ideaRepository = IdeaRepository;
+    }
+
+    async getIdeaComments(ideaId){
+        if(!ideaId){
+            const error = new Error();
+            error.status = 400;
+            error.message = 'ideaId must be sent';
+            throw error;
+        }
+
+        const idea = await _ideaRepository.get(ideaId);
+
+        if(!ideaId){
+            const error = new Error();
+            error.status = 400;
+            error.message = 'ideaId must be sent';
+            throw error;
+        }
+
+        const { comments } = idea;
+        return comments;
+    }
+
+    async createComment(comment, ideaId){
+        if(!ideaId){
+            const error = new Error();
+            error.status = 400;
+            error.message = 'ideaId must be sent';
+            throw error;
+        }
+
+        const idea = await _ideaRepository.get(ideaId);
+
+        if(!ideaId){
+            const error = new Error();
+            error.status = 400;
+            error.message = 'ideaId must be sent';
+            throw error;
+        }
+
+        const createComment = await _commetRepository.create(comment);
+        idea.comment.push(createComment);
+
+        return await _ideaRepository.update(ideaId, { comment: idea.comment });
+
+    }
+};
+
+module.exports = CommnetService;
