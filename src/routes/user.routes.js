e0c84891df404ca9aaefc76b97f6765456718b80
JochyDev <joseluisjimenez1998@gmail.com>
2022-10-27 13:13:18 -0400

    ADD: cache time implemented

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,16 @@
 const { Router } = require('express');
-const { AuthMiddleware } = require('../middlewares')
+const { AuthMiddleware, ParseIntMiddleware, CacheTimeMiddleware } = require('../middlewares');
+const { CACHE_TIME } = require('../helpers')
 
 
 module.exports = function({UserController}){
     const router = Router();
 
-    router.get('', [AuthMiddleware] , UserController.getAll),
+    router.get('', [
+        AuthMiddleware, 
+        ParseIntMiddleware,
+        CacheTimeMiddleware(CACHE_TIME.ONE_HOUR) 
+    ], UserController.getAll),
     router.get('/:userId', UserController.get),
     router.patch('/:userId', UserController.update),
     router.delete('/:userId', UserController.delete)
